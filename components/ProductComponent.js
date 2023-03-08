f6807060a6527b50449ba28596efebcf49218cec
danknessdra <danknessdra@gmail.com>
2023-03-08 14:15:23 -0800

    democard

--- components/ProductComponent.js
+++ components/ProductComponent.js
@@ -3,11 +3,17 @@
 export default function ProductComponent(props) {
 
 
-  // Check if there's an error and set the button text accordingly
-  
+
 
   return (
-    <div>test</div>
+    <div class="card" style="width: 18rem;">
+        <img src="https://cdn.pixabay.com/photo/2017/09/25/13/12/puppy-2785074__340.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
 
   );
 }
