c27c32126f5ff86a59d8f2cb761524e14bf5c6da
danknessdra <danknessdra@gmail.com>
2023-03-08 22:03:08 -0800

    card rendered but it looks terrible

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -4,12 +4,12 @@ import React, { Component } from 'react'
 export default class ProductComponent extends Component {
   render() {
     return (
-      <div class="card" style="width: 18rem;">
-        <img src="https://cdn.pixabay.com/photo/2017/09/25/13/12/puppy-2785074__340.jpg" class="card-img-top" alt="..."></img>
-        <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+      <div className="card">
+        <img src="https://cdn.pixabay.com/photo/2017/09/25/13/12/puppy-2785074__340.jpg"></img>
+        <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
     </div>
 </div>
     )
