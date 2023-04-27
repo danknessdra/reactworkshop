c8c048900be3a2a11c4c428258d213f8ba3e1194
danknessdra <danknessdra@gmail.com>
2023-04-27 12:22:56 -0700

    almost almost almost

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import ShelfComponent from './components/ShelfComponent';
 
 function App() {
   return (
+    
     <div>
+      <div class = "shop">Shop<br></br>
+      Counter: 
+      
+      </div>
       <ShelfComponent></ShelfComponent>
       <ShelfComponent></ShelfComponent>
       <ShelfComponent></ShelfComponent>
--- src/components/ShelfComponent.js
+++ src/components/ShelfComponent.js
@@ -1,11 +1,12 @@
 
 import React, { Component } from 'react'
 import ProductComponent from './ProductComponent'
-
+import React, { Children } from "react";
 export default class ShelfComponent extends Component {
   render() {
     return (
       <div class =  "row">
+        <div class = "col-md-1"></div>
         <div class="col-md-2"><ProductComponent></ProductComponent></div>
         <div class="col-md-2"><ProductComponent></ProductComponent></div>
       </div>
